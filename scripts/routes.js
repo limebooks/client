9791689f85b28e71974c7bd56749fd3c837dfa00
keitheck <eckertkeith@gmail.com>
2018-01-25 09:29:08 -0800

    book view route added

--- scripts/routes.js
+++ scripts/routes.js
@@ -2,8 +2,9 @@
 
 page('/new', entryView.init);
 page('/', allBooks.init);
+
 // page('/about', aboutView.init);
-// page('/books/:book_id', bookView.init);
+page('/books/:book_id', bookView.init);
 // page('/books/new', addBookView.init);
 
 
